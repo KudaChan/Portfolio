6ab5740aab20afe02e83d4c08b5a4639c4956404
KudaChan <kumarnchandan05@gmail.com>
2024-04-20 21:58:27 +0530

    todays progress

--- AI_ML_DS/Minor_Project_Ai_Plant_Recognition/Minor_Project_Ai_Plant_Recognition/Program.cs
+++ AI_ML_DS/Minor_Project_Ai_Plant_Recognition/Minor_Project_Ai_Plant_Recognition/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Minor_Project_Ai_Plant_Recognition.SorceCode.Preprocessing;
+using System.Drawing;
 
 namespace Minor_Project_Ai_Plant_Recognition
 {
@@ -9,11 +10,22 @@ namespace Minor_Project_Ai_Plant_Recognition
             // Directory of orignal dataset
             string path = "D:\\Project\\AI_ML_DS\\Minor_Project_Ai_Plant_Recognition\\Minor_Project_Ai_Plant_Recognition\\Data\\Dataset(test)";
             string pathTextFile = "D:\\Project\\AI_ML_DS\\Minor_Project_Ai_Plant_Recognition\\Minor_Project_Ai_Plant_Recognition\\Temp\\dataset";
+
+            string[] action = ["resize", "Augment"];
+
             ImageAccess imageAccess = new ImageAccess();
-            imageAccess.DirectoryParser(path, pathTextFile);
 
             ImageResize imageResize = new ImageResize();
+
+            DataAugmentation augmentation = new DataAugmentation();
+
+            imageAccess.DirectoryParser(path, pathTextFile, action[0]);
             imageResize.ResizeFactory(pathTextFile);
+
+            string pathResized = "D:\\Project\\AI_ML_DS\\Minor_Project_Ai_Plant_Recognition\\Minor_Project_Ai_Plant_Recognition\\Data\\Dataset(resized)";
+            imageAccess.DirectoryParser(pathResized, pathTextFile, action[1]);
+
+            augmentation.AugmentFactory(pathTextFile);
         }
     }
 }
--- AI_ML_DS/Minor_Project_Ai_Plant_Recognition/Minor_Project_Ai_Plant_Recognition/SorceCode/DataPreprocessing.cs
+++ AI_ML_DS/Minor_Project_Ai_Plant_Recognition/Minor_Project_Ai_Plant_Recognition/SorceCode/DataPreprocessing.cs
@@ -1,7 +1,6 @@
 ﻿using Emgu.CV;
 using Emgu.CV.CvEnum;
 using System.Drawing;
-using System.IO;
 
 namespace Minor_Project_Ai_Plant_Recognition.SorceCode.Preprocessing
 {
@@ -16,13 +15,11 @@ namespace Minor_Project_Ai_Plant_Recognition.SorceCode.Preprocessing
         /// </summary>
         /// <param name="path">The base path of the directories.</param>
         /// <param name="pathTextFile">The path of the text file to write to.</param>
-        public void DirectoryParser(string path, string pathTextFile)
+        public void DirectoryParser(string path, string pathTextFile, string action)
         {
-            string[] directories = new string[]
-            {
-                    Path.Combine(path, "Medicinal_Leaf_dataset"),
-                    Path.Combine(path, "Medicinal_plant_dataset")
-            };
+            string[] directories;
+            List<string> directoriesList = pathMakerBasedOnAction(path, action);
+            directories = directoriesList.ToArray();
 
             Directory.CreateDirectory(pathTextFile);
 
@@ -48,6 +45,41 @@ namespace Minor_Project_Ai_Plant_Recognition.SorceCode.Preprocessing
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Constructs a list of directories based on the specified action.
+        /// </summary>
+        /// <param name="path">The base path to use when constructing the directories.</param>
+        /// <param name="action">The action to base the directories on. Can be "resize", "parse", or "Augment".</param>
+        /// <returns>A list of directories constructed based on the specified action.</returns>
+        public List<string> pathMakerBasedOnAction(string path, string action)
+        {
+            List<string> directories = new List<string>();
+
+            if (action == "resize" || action == "parse")
+            {
+                directories.Add(Path.Combine(path, "Medicinal_Leaf_dataset"));
+                directories.Add(Path.Combine(path, "Medicinal_plant_dataset"));
+            }
+            else if (action == "Augment")
+            {
+                directories.Add(Path.Combine(path, "size224_224"));
+                directories.Add(Path.Combine(path, "size299_299"));
+
+                foreach (string directory in directories.ToList()) // Use ToList to create a copy for iteration
+                {
+                    directories.AddRange(pathMakerBasedOnAction(directory, "parse"));
+                }
+            }
+
+            Console.WriteLine("in augment: ");
+            foreach (string directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            return directories;
         }
     }
 
@@ -170,5 +202,116 @@ namespace Minor_Project_Ai_Plant_Recognition.SorceCode.Preprocessing
     /// </summary>
     internal class DataAugmentation
     {
+        private readonly NewImageWrite _newImageWrite = new NewImageWrite();
+
+        /// <summary>
+        /// Augments the images specified in the text file at the given path.
+        /// </summary>
+        /// <param name="path">The path of the text file containing the image paths.</param>
+        public void AugmentFactory(string path)
+        {
+            using (StreamReader reader = new StreamReader(Path.Combine(path, "dataset.txt")))
+            {
+                string line;
+                while ((line = reader.ReadLine()!) != null)
+                {
+                    FlipAugmenter(line);
+                    RotateAugmenter(line);
+                    NoiseAugmenter(line);
+                    TranslateAugmenter(line);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Augments an image by flipping it.
+        /// </summary>
+        /// <param name="path">The path of the image to augment.</param>
+        private void FlipAugmenter(string path)
+        {
+            string action = "flipped";
+            Mat image = CvInvoke.Imread(path, ImreadModes.Color);
+            Mat flippedImage = new Mat();
+            CvInvoke.Flip(image, flippedImage, FlipType.Both);
+
+            ImageWriterAssistance(path, flippedImage, action);
+        }
+
+        /// <summary>
+        /// Augments an image by rotating it.
+        /// </summary>
+        /// <param name="path">The path of the image to augment.</param>
+        private void RotateAugmenter(string path)
+        {
+            string action = "rotated";
+            Mat image = CvInvoke.Imread(path, ImreadModes.Color);
+            Mat rotatedImage = new Mat();
+            CvInvoke.Rotate(image, rotatedImage, RotateFlags.Rotate90Clockwise);
+
+            ImageWriterAssistance(path, rotatedImage, action);
+        }
+
+        /// <summary>
+        /// Augments an image by adding noise to it.
+        /// </summary>
+        /// <param name="path">The path of the image to augment.</param>
+        private void NoiseAugmenter(string path)
+        {
+            string action = "noised";
+            Mat image = CvInvoke.Imread(path, ImreadModes.Color);
+            Mat noisedImage = new Mat();
+            CvInvoke.GaussianBlur(image, noisedImage, new Size(5, 5), 0);
+
+            ImageWriterAssistance(path, noisedImage, action);
+        }
+
+        /// <summary>
+        /// Augments an image by translating it.
+        /// </summary>
+        /// <param name="path">The path of the image to augment.</param>
+        private void TranslateAugmenter(string path)
+        {
+            string action = "translated";
+            Mat image = CvInvoke.Imread(path, ImreadModes.Color);
+            int imageWidth = image.Width;
+            int imageHeight = image.Height;
+            Mat translatedImage = new Mat();
+            Matrix<float> shiftMatrix = new Matrix<float>(2, 3);
+            Random rand = new Random();
+            int shiftX = rand.Next(-50, 50);  // Random shift in x direction between -50 and 50
+            int shiftY = rand.Next(-50, 50);  // Random shift in y direction between -50 and 50
+
+            shiftMatrix[0, 0] = 1;  // Identity element
+            shiftMatrix[1, 1] = 1;  // Identity element
+            shiftMatrix[0, 2] = shiftX;  // Shift in x direction
+            shiftMatrix[1, 2] = shiftY;  // Shift in y direction
+            CvInvoke.WarpAffine(image, translatedImage, shiftMatrix, new Size(imageWidth, imageHeight), 0);
+
+            ImageWriterAssistance(path, translatedImage, action);
+        }
+
+        /// <summary>
+        /// The directory to output the augmented images to.
+        /// </summary>
+        public string OutputDirectory { get; } = "D:\\Project\\AI_ML_DS\\Minor_Project_Ai_Plant_Recognition\\Minor_Project_Ai_Plant_Recognition\\Data\\Dataset(augmented)";
+
+        /// <summary>
+        /// Assists in writing the augmented image to the output directory.
+        /// </summary>
+        /// <param name="path">The path of the original image.</param>
+        /// <param name="newImg">The augmented image.</param>
+        private void ImageWriterAssistance(string path, Mat newImg, string action)
+        {
+            DirectoryInfo? directoryParentParentInfo = new DirectoryInfo(path).Parent?.Parent?.Parent;
+            DirectoryInfo? dirParentInfo = new DirectoryInfo(path).Parent?.Parent;
+            DirectoryInfo? dirInfo = new DirectoryInfo(path).Parent;
+            string? dirName = dirInfo?.Name!;
+            string? dirParentName = dirParentInfo?.Name!;
+            string? dirParentParentName = directoryParentParentInfo?.Name!;
+            string imgName = Path.GetFileName(path);
+            string specificOutputDirectory = Path.Combine(OutputDirectory, action, dirParentParentName, dirParentName, dirName);
+
+            _newImageWrite.DirrectoryCreate(specificOutputDirectory, newImg, imgName);
+        }
     }
 }
